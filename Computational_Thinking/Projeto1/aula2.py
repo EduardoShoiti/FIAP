f017e06a6dda58915d29d689c78ec2a7b85763ba
Eduardo Shoiti <shoitiaragaki@gmail.com>
2023-02-28 19:44:58 -0300

    Computational Thinking - aula 1

---
+++ Computational_Thinking/Projeto1/aula2.py
@@ -0,0 +1,11 @@
+Idade_Alvo = 65
+Minha_idade = 18
+
+print("Formatando idade alvo {}".format(Idade_Alvo))
+
+print(f"Segunda forma {Idade_Alvo}")
+
+print("Terceira forma {i}, outro exemplo {n}".format(i=Idade_Alvo, n=Minha_idade))
+
+# outros tipos de casting
+
